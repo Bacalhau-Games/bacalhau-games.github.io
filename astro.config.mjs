daa6e2af2444d9f3b317d2b7efbd7b45b07ad2e8
Farious <fabio.reis@bacalhau.games>
2025-06-13 00:47:57 +0100

    Retest

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import compress from 'astro-compress';
+
+// https://astro.build/config
+export default defineConfig({
+  experimental: {
+    viewTransitions: true,
+  },
+  integrations: [tailwind(), compress()],
+  site: 'https://bacalhau-games.github.io/',
+  trailingSlash: "never"
+});
